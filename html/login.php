c3dde0c79e3ffa68b80ebe67eeba220f9c7a5538
Abu Zharr Luqman bin Abdul Aziz <32317099+panjang007@users.noreply.github.com>
2023-07-06 00:18:31 +0800

    Update login.php
    aku tambah error messsage dekat placeholder

--- html/login.php
+++ html/login.php
@@ -4,11 +4,23 @@ session_start();
 
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Check if user has submitted the form
 
+    // Check if user has submitted the form
+ 
+    $conn = mysqli_connect($database_host, $database_user, $database_password, $database_name);
     if (isset($_POST['username']) && isset($_POST['password'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
+
+        // Whitelist validation using regex
+        $username_pattern = '/^[a-zA-Z0-9_]{3,20}$/'; // Allow alphanumeric and underscore, 3-20 characters
+        $password_pattern = '/^[a-zA-Z0-9!@#$%^&*()]{8,}$/'; // Allow alphanumeric and some special characters, minimum 8 characters
+
+        // Validate username against whitelist pattern
+        if (!preg_match($username_pattern, $username)) {
+            header('Location: loginGP.html?error=Invalid username format.');
+            exit();
+        }
 
         // Retrieve user from database
         $sql = "SELECT * FROM users WHERE username='$username'";
@@ -19,16 +31,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $row = mysqli_fetch_assoc($result);
             $db_password = $row['password'];
 
+            // Validate password against whitelist pattern
+            if (!preg_match($password_pattern, $password)) {
+                header('Location: loginGP.html?error=Invalid password format.');
+                exit();
+            }
+
             // Verify password
             if (password_verify($password, $db_password)) {
                 // Login successful, generate random session ID
                 $session_id = bin2hex(random_bytes(16));
-                echo "Session ID: " . $session_id . "<br>";
 
                 // Disable output buffering
-                // while (ob_get_level()) {
-                //     ob_end_flush();
-                // }
+                while (ob_get_level()) {
+                    ob_end_flush();
+                }
 
                 // Save user session with random session ID and last activity time
                 $_SESSION['username'] = $username;
@@ -43,12 +60,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 }
                 exit();
             } else {
-                // Login failed, display error message
-                header('Location: loginGP.html?error=Invalid username or password.');
+                // Wrong password, display error message
+                header('Location: loginGP.html?error=Wrong password.');
                 exit();
             }
         } else {
-            // Login failed, display error message
+            // No username exists, display error message
             header('Location: loginGP.html?error=Invalid username.');
             exit();
         }
@@ -71,7 +88,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 // Session is idle, destroy it and redirect to login page
                 session_unset();
                 session_destroy();
-                session_write_close(); 
                 header("Location: loginGP.html?error=Session expired due to inactivity.");
                 exit();
             } else {
