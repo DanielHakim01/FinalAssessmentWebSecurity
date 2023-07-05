7512c2b6f813bc80c0674547ee8ec88d432367d2
octane23 <95738756+octane23@users.noreply.github.com>
2023-07-05 22:44:04 +0800

    Add files via upload

---
+++ html/security.php
@@ -0,0 +1,15 @@
+<?php
+header("X-Frame-Options: DENY");
+header("Content-Security-Policy: default-src 'self';");
+
+function generateCSRFToken() {
+    if (empty($_SESSION['csrf_token'])) {
+      if (function_exists('random_bytes')) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+      } else {
+        $_SESSION['csrf_token'] = bin2hex(openssl_random_pseudo_bytes(32));
+      }
+    }
+    return $_SESSION['csrf_token'];
+}
+?>
