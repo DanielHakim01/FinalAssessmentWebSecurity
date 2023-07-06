a716693bc8b8f5fcf1fb18a38d89938391b21a0e
kreyton01 <47686304+kreyton01@users.noreply.github.com>
2023-07-06 23:39:16 +0800

    Update home.php

--- html/home.php
+++ html/home.php
@@ -6,21 +6,96 @@ require_once('../html/idle.php');
 <html lang="en">
 <head>
   <title>IIUM SPORT RESERVATION  CENTRE</title>
-  <link rel="stylesheet" href="../CSS_JS/navmenu.css">
+  <link rel="stylesheet" href="../CSS_JS/faciinfo.css">
+  <style>
+    body {
+      background-color: #f1f1f1;
+      color: #1b9284;
+      font-family: Arial, sans-serif;
+    }
 
+    .button {
+      background-color: #1b9284;
+      border: none;
+      color: rgb(255, 255, 255);
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      border-radius: 25px;
+    }
+
+    .main {
+      text-align: center;
+      margin-top: 30px;
+    }
+
+    .pill-nav {
+      background-color: #f1f1f1;
+      overflow: hidden;
+    }
+
+    .pill-nav a {
+      color: #1b9284;
+      float: left;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+      font-size: 17px;
+    }
+
+    .pill-nav a.active {
+      background-color: #1b9284;
+      color: white;
+    }
+
+    .pill-nav a:hover {
+      background-color: #ddd;
+      color: #1b9284;
+    }
+
+    h1 {
+      color: #1b9284;
+    }
+
+    .row {
+      display: flex;
+      flex-wrap: wrap;
+      padding: 0 4px;
+    }
+
+    .column {
+      flex: 25%;
+      max-width: 25%;
+      padding: 0 4px;
+    }
+
+    .column .content {
+      background-color: white;
+      margin-bottom: 15px;
+      padding: 15px;
+      text-align: center;
+    }
+
+    .content h3 {
+      color: #1b9284;
+      text-align: center;
+    }
+  </style>
 </head>
 
 <body>
 
-<div class="pill-nav">
-  <a href="Main_Page.html"class = "active">Home</a>
-  <a href="menuGP.php">Menu</a>
-  <a href="faci_info.php">Facilities</a>
-  <a href="logout.php">Logout</a>
-</div>
-
+<div class="main">
+    <div class="pill-nav">
+      <a style="color: white;" href="home.php"  class="active">Home</a>
+      <a style="color: #1b9284;" href="menuGP.php">Menu</a>
+      <a style="color: #1b9284;" href="faci_info.php">Facilities</a>
+      <a style="color: #1b9284;" href="logout.php">Logout</a>
+    </div>
 <centre>
-<h1 class="box">IIUM One Sport Centre</h1>
+<h1 >IIUM SPORT CENTRE</h1>
 </centre>
 
 </body>
