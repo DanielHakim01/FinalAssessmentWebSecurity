65b6d86f70b45e0785b8cc8965c2cc0e2a4ab15d
Abu Zharr Luqman bin Abdul Aziz <32317099+panjang007@users.noreply.github.com>
2023-07-06 16:48:34 +0800

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,6 +1,6 @@
 <?php
 $database_host = 'localhost';
-$database_user = 'panjang007';
-$database_password = 'UmmuZharr1';
+$database_user = 'root';
+$database_password = '';
 $database_name = 'user_info';
 ?>
