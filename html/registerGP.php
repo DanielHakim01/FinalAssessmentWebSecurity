618856509e0ca74c8be322a77188a40ee9f42e84
kreyton01 <47686304+kreyton01@users.noreply.github.com>
2023-07-06 23:36:08 +0800

    Update registerGP.php

--- html/registerGP.php
+++ html/registerGP.php
@@ -1,53 +1,24 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>User Registration</title>
-    <link rel="stylesheet" href="../CSS_JS/loginGP.css" />
-  </head>
-
-  <body>
-  <h2>Register</h2>
-    <div class="box">
-
-      <div class="inner-box">
-
-          <form class="register" action="registerGP.php" method="post">
-            <div class="form-group">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username"  pattern="[a-z0-9]+" required>
-            <span id="username-message" class="text-danger"></span>
-            </div>
-
-            <div class="form-group">
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" minlength="8" required>
-            <span id="password-message" class="text-danger"></span>
-            </div>
-
-            <input type="submit" class="btn btn-primary" value="Register"></button>
-            <p>Already a Member?</p>
-            <a href="loginGP.html"> Log In </a>
-        </form>
-
-      </div>
-
-    </div>
-  </body>
-</html>
 
 <?php
+require_once('../app/config.php');
 session_start();
 // connect to database
-$database_host = 'localhost';
-$database_user = 'root';
-$database_password = '';
-$database_name = 'user_info';
 
 // connect to the database
 $conn = mysqli_connect($database_host, $database_user, $database_password, $database_name);
 
+if (!isset($_SESSION['csrf_token'])) {
+  $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
 // check if user has submitted the form
-if(isset($_POST['username']) && isset($_POST['password'])) {
+if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['csrf_token'])) {
+    // Validate CSRF token
+    if ($_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+        // Show error message and redirect to registration page
+        echo "<script>showError('Error: Invalid CSRF token.');</script>";
+        exit();
+    }   
+    
     $username = $_POST['username'];
     $password = $_POST['password'];
 
@@ -57,7 +28,7 @@ if(isset($_POST['username']) && isset($_POST['password'])) {
 
     if (mysqli_num_rows($result_check) > 0) {
         // Show error message and redirect to registration page
-        echo '<script>alert("Username already exists. Please choose a different username."); window.location.href = "registerGP.php";</script>';
+        echo "<script>showError('Error: Username already exists. Please choose a different username.');</script>";
       } elseif (strlen($password) < 8) {
         // Show error message and redirect to registration page
         echo "<script>showError('Error: Password must be at least 8 characters long. Please try again.');</script>";
@@ -74,7 +45,7 @@ if(isset($_POST['username']) && isset($_POST['password'])) {
           // Get the generated ID
           $id = mysqli_insert_id($conn);
           // Registration successful, display success message and redirect to login page
-          echo '<script>alert("User registration successful! Please login."); window.location.href = "loginGP.html";</script>' ;
+          echo "<script>alert('User registration successful! Please login.'); window.location.href = 'loginGP.html';</script>";
           exit();
       }
        else {
@@ -83,3 +54,42 @@ if(isset($_POST['username']) && isset($_POST['password'])) {
         }
       }
     }
+    ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>User Registration</title>
+    <link rel="stylesheet" href="../CSS_JS/loginGP.css" />
+    <script src="../CSS_JS/loginGP.js" charset="utf-8"></script>
+ 
+  </head>
+
+  <body>
+    <form class="box" action="registerGP.php" method="post">
+      <h1>CREATE AN ACCOUNT</h1>
+
+           <label for="username">Username:</label>
+            <input type="text" id="username" name="username"  pattern="[a-z0-9]+" required><br><br>
+      
+      
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" minlength="8" required><br><br>
+      
+    
+            <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+
+            <div class="password"><br>
+      
+           </div>
+          
+      
+            <input type="submit" class="btn btn-primary" value="Register"></button>
+      
+             <div class="signup_link">
+             Already a member?<a href="loginGP.html"> Log In </a>
+             </div>
+
+    </form>
+  </body>
+</html>
+
