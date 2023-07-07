c55ceac5f2708a6043cff478f12b519e3c8f9d6f
Abu Zharr Luqman bin Abdul Aziz <32317099+panjang007@users.noreply.github.com>
2023-07-07 11:09:01 +0800

    fix csp error

--- html/menuGP.php
+++ html/menuGP.php
@@ -12,96 +12,19 @@ if (isset($_SESSION['csrf_token'])) {
 }
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <title>MENU</title>
   <link rel="stylesheet" href="../CSS_JS/faciinfo.css">
-  <style>
-    body {
-      background-color: #f1f1f1;
-      color: #1b9284;
-      font-family: Arial, sans-serif;
-    }
-
-    .button {
-      background-color: #1b9284;
-      border: none;
-      color: rgb(255, 255, 255);
-      padding: 15px 32px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-      border-radius: 25px;
-    }
-
-    .main {
-      text-align: center;
-      margin-top: 30px;
-    }
-
-    .pill-nav {
-      background-color: #f1f1f1;
-      overflow: hidden;
-    }
-
-    .pill-nav a {
-      color: #1b9284;
-      float: left;
-      text-align: center;
-      padding: 14px 16px;
-      text-decoration: none;
-      font-size: 17px;
-    }
-
-    .pill-nav a.active {
-      background-color: #1b9284;
-      color: white;
-    }
-
-    .pill-nav a:hover {
-      background-color: #ddd;
-      color: #1b9284;
-    }
-
-    h1 {
-      color: #1b9284;
-    }
-
-    .row {
-      display: flex;
-      flex-wrap: wrap;
-      padding: 0 4px;
-    }
-
-    .column {
-      flex: 25%;
-      max-width: 25%;
-      padding: 0 4px;
-    }
-
-    .column .content {
-      background-color: white;
-      margin-bottom: 15px;
-      padding: 15px;
-      text-align: center;
-    }
-
-    .content h3 {
-      color: #1b9284;
-      text-align: center;
-    }
-  </style>
+  <link rel="stylesheet" href="../CSS_JS/menuStyle.css">
 </head>
 
 <body>
   <div class="main">
     <div class="pill-nav">
-      <a style="color: #1b9284;" href="home.php">Home</a>;
+      <a id="homeNavTitle" href="home.php">Home</a>;
       <a style="color: white;" href="menuGP.php" class="active">Menu</a>
       <a style="color: #1b9284;" href="faci_info.php">Facilities</a>
       <a style="color: #1b9284;" href="logout.php">Logout</a>
